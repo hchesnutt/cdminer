15683447e8b874a04bc2ff6381a49b092bac7d22
hchesnutt <t.henry.chesnutt@gmail.com>
2018-07-11 16:30:05 -0700

    add testing dir

--- helpers.test.js
+++
@@ -1,5 +0,0 @@
-// parseArgs should throw an exception if more or less than 3 arguments are submitted
-// parseArgs should throw an exception if path is not a path
-// parseArgs should throw an exception if path cannot be resolved
-// parseArgs should throw an exception if query is not valid
-// parseArgs should throw an exception if query parameter is not valid
