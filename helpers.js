2ecd98b869ec878480e3d84401438948d5b5e0f6
hchesnutt <t.henry.chesnutt@gmail.com>
2018-07-10 20:12:52 -0700

    finish validateArgs

--- helpers.js
+++ helpers.js
@@ -1,4 +1,7 @@
 const chalk = require('chalk');
+const fs = require('fs');
+
+const queries = require('./queries.js');
 
 function handleError(message) {
   console.log(chalk.red(message));
@@ -9,6 +12,14 @@ module.exports = {
   parseArgs: (argv) => {
     const args = argv.slice(2);
     if (args.length > 3 || args.length < 3) handleError(`Expected 3 arguments, received ${args.length}`)
-    const [ path, query, params ] = args;
+    const [ path, query, param ] = args;
+    return { path, query, param };
+  },
+  validateArgs: ({ path, query, param }) => {
+    // path should resolve
+    if (!fs.existsSync(path)) handleError(`${path} could not be resolved`);
+    // query should be an allowable query
+    if (!queries[query]) handleError(`${query} is not a known query`);
   }
+
 }
