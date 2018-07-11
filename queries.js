fa4a4ec995cbd691b50b1f5e9968b95ff25d3780
hchesnutt <t.henry.chesnutt@gmail.com>
2018-07-10 20:08:21 -0700

    wireframe queries

---
+++ queries.js
@@ -0,0 +1,17 @@
+module.exports = {
+  locate: () => {
+
+  },
+  find_before: () => {
+
+  },
+  find_after: () => {
+
+  },
+  find_companies_between_size: () => {
+
+  },
+  find_type: () => {
+
+  },
+};
