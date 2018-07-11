a0e9f500a377b97cd1b384558a56cee1ff1c3b47
hchesnutt <t.henry.chesnutt@gmail.com>
2018-07-10 19:46:58 -0700

    add helpers and helpers test

--- helpers.js
+++ helpers.js
@@ -1,5 +1,14 @@
+const chalk = require('chalk');
+
+function handleError(message) {
+  console.log(chalk.red(message));
+  process.exit();
+}
+
 module.exports = {
   parseArgs: (argv) => {
-    
+    const args = argv.slice(2);
+    if (args.length > 3 || args.length < 3) handleError(`Expected 3 arguments, received ${args.length}`)
+    const [ path, query, params ] = args;
   }
 }
