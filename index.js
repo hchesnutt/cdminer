15951ca30be19a082933bff986b622db27c3499d
hchesnutt <t.henry.chesnutt@gmail.com>
2018-07-12 14:26:50 -0700

    orchesrate entrypoint logic

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const chalk = require('chalk');
 const { parseArgs, loadData } = require('./lib');
 const { filterCompanies } = require('./queries');
 
+// Collect and parse stdin
 const args = parseArgs(process.argv);
 const { filePath, query, param } = args;
 
+// Load data from file
 const companies = loadData(filePath);
 
+// Execute query filtering operation
 const companyNames = filterCompanies(companies, query, param);
 
+// Print results to stdout
 console.log(`
   Company Names:
   ${companyNames.toString()}
