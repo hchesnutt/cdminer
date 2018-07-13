3cb2ea2abc48de13dd0843d60c959598e02b58be
hchesnutt <t.henry.chesnutt@gmail.com>
2018-07-12 17:28:04 -0700

    add all query tests

--- spec/queries.test.js
+++ spec/queries.test.js
@@ -11,7 +11,7 @@ const companies = require('./testData/valid.json');
 
 describe('Query functions', function() {
   describe('locate', function() {
-    it('Should return an array of filtered states', function() {
+    it.only('Should return an array of filtered states', function() {
       expect(locate(companies, 'DC')).to.be.an('array');
       expect(locate(companies, 'DC').length).to.equal(1);
       expect(locate(companies, 'DC')[0].state).to.equal('DC');
@@ -19,31 +19,45 @@ describe('Query functions', function() {
   });
 
   describe('find_before', function() {
-    it.only('Should return an array of companies with filter applied', function() {
-      expect(find_before(companies, '1-10')).to.be.an('array');
-      expect(find_before(companies, '1-10').length).to.equal(1);
+    it.only('Should return an array of companies less than or equal to input year', function() {
+      expect(find_before(companies, 2013)).to.be.an('array');
+      expect(find_before(companies, 2013).length).to.equal(1);
 
-      expect(find_before(companies, '1-10')[0].full_time_employees).to.equal('1-10');
+      expect(find_before(companies, 2013)[0].year_founded).to.equal(2010);
     });
     it.only('Should be inclusive', function() {
-      expect(find_before(companies, '51-200').length).to.equal(2);
+      expect(find_before(companies, 2014).length).to.equal(2);
     });
   });
   
   describe('find_after', function() {
-    it.only('Should return an array of companies with filter applied', function() {
-      expect(find_after(companies, '51-200')).to.be.an('array');
-      expect(find_after(companies, '51-200').length).to.equal(1);
-      expect(find_after(companies, '51-200')[0].full_time_employees).to.equal('51-200');
+    it.only('Should return an array of companies greater than or equal to input year', function() {
+      expect(find_after(companies, 2011)).to.be.an('array');
+      expect(find_after(companies, 2011).length).to.equal(1);
+      expect(find_after(companies, 2011)[0].year_founded).to.equal(2014);
     });
     it.only('Should be inclusive', function() {
-      expect(find_after(companies, '1-10').length).to.equal(2);
+      expect(find_after(companies, 2010).length).to.equal(2);
     });
   });
   
   describe('find_companies_between_size', function() {
-    it('Shoudl return an array of companies at the input size', function() {
+    it.only('Should return an array of companies at the input size', function() {
+      expect(find_companies_between_size(companies, '51-200')).to.be.an('array');
+      expect(find_companies_between_size(companies, '51-200').length).to.equal(1);
+      expect(find_companies_between_size(companies, '51-200')[0].full_time_employees).to.equal('51-200');
+    });
+  });
 
+  describe('find_type', function() {
+    it.only('Should return an array of companies at the input size', function() {
+      expect(find_type(companies, 'Data/Technology')).to.be.an('array');
+      expect(find_type(companies, 'Data/Technology').length).to.equal(1);
+      expect(find_type(companies, 'Data/Technology')[0].company_category).to.equal('Data/Technology');
+    });
+    it.only('Should not include companies with "NA" or "N/A" company_category', function() {
+      companies.push({ company_category: 'NA' }, { company_category: 'N/A' });
+      expect(find_type(companies, 'Data/Technology').length).to.equal(1);
     });
   });
 });
